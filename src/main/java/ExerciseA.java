bccc97658d608f9154314cadd2fb965005a21a9c
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2019-03-15 13:18:38 +0200

    Optimized tetris piece generation.

--- src/main/java/ExerciseA.java
+++ src/main/java/ExerciseA.java
@@ -10,20 +10,20 @@ class ExerciseA {
             InputStream is = new FileInputStream("InputA.txt");
             BufferedInputStream bf = new BufferedInputStream(is);
             Scanner scanner = new Scanner(bf);
-            while(scanner.hasNext()){
+            while (scanner.hasNext()) {
                 int tetrisColumns = scanner.nextInt();
                 int tetrisPieces = scanner.nextInt();
 
                 TetrisTable tetrisTable = new TetrisTable(tetrisColumns);
-                for(int i = 0; i < tetrisPieces; i++){
+                for (int i = 0; i < tetrisPieces; i++) {
                     int pieceId = scanner.nextInt();
                     int pieceRotation = scanner.nextInt();
                     int pieceTranslation = scanner.nextInt();
 
-                    TetrisPiece tetrisPiece = new TetrisPiece(pieceId, pieceRotation);
+                    TetrisPiece tetrisPiece = createTetrisPiece(pieceId, pieceRotation);
                     tetrisTable.addPiece(tetrisPiece, pieceTranslation - 1);
-                }     
-                tetrisTable.print();           
+                }
+                tetrisTable.print();
             }
             scanner.close();
         } catch (FileNotFoundException ex) {
@@ -31,5 +31,17 @@ class ExerciseA {
             // insert code to run when exception occurs
         }
 
+    }
+
+    private final static HashMap<String, TetrisPiece> pieceMap = new HashMap<>();
+
+    private static TetrisPiece createTetrisPiece(int pieceId, int pieceRotation) {
+        String pieceKey = pieceId + " " + pieceRotation;
+        TetrisPiece tetrisPiece = pieceMap.get(pieceKey);
+        if (tetrisPiece == null) {
+            tetrisPiece = new TetrisPiece(pieceId, pieceRotation);
+            pieceMap.put(pieceKey, tetrisPiece);
+        }
+        return tetrisPiece;
     }
 }
